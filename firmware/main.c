a9b8bf0a3def367671cb424df0d34fd0e7e73948
luisan00 <luisan00@hotmail.com>
2021-09-23 14:39:55 +0200

    documentation: update license header

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,6 @@
 /* 
- * Copyright (c) 2021 Mesh4all <mesh4all.org> 
+ * Copyright (c) 2021 Mesh4all <mesh4all.org>
+ 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
--- tests/test_driver_epd-bw-spi/main.c
+++ tests/test_driver_epd-bw-spi/main.c
@@ -1,5 +1,6 @@
-/* Copyright 2021 luisan00 <luisan00@hotmail.com>
- *
+/* 
+ * Copyright (c) 2021 Mesh4all <mesh4all.org>
+ 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * @author      luisan00 <luisan00@hotmail.com>
  */
 
 #include <stdio.h>
