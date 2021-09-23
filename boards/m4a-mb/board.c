e050c6e339ad16790ffb4f7484fb17b1d4f1aa97
luisan00 <luisan00@hotmail.com>
2021-09-23 13:28:42 +0200

    doc: update license header

--- boards/m4a-mb/board.c
+++ boards/m4a-mb/board.c
@@ -1,9 +1,19 @@
-/*
- * Copyright (C) 2021 Mesh4All <mesh4all.org>
+/* 
+ * Copyright (c) 2021 Mesh4all <mesh4all.org>
+ 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file is subject to the terms and conditions of the Apache-2.0 license.
- *  See the file LICENSE in the top level directory for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 /**
  * @ingroup         boards_m4a_mb
  * @brief           Board specific implementations for M4A-MB
