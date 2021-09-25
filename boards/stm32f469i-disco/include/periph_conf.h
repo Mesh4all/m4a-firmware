23c3b7d9e924b09e792f8a56dfaae717c333a295
luisan00 <luisan00@hotmail.com>
2021-09-25 15:46:43 +0200

    Board/stm32f469i disco (#17)
    * periph: change uart pin,  add test for uart periph
    * delete some older tests

--- boards/stm32f469i-disco/include/periph_conf.h
+++ boards/stm32f469i-disco/include/periph_conf.h
@@ -52,47 +52,46 @@ extern "C"
 {
 #endif
 
-  /**
-    * @name    UART configuration
-    * 
-    */
-  static const uart_conf_t uart_config[] = {
-      // CN1, VCP
-      {.dev = USART3,
-       .rcc_mask = RCC_APB1ENR_USART3EN,
-       .rx_pin = GPIO_PIN(PORT_B, 11),
-       .tx_pin = GPIO_PIN(PORT_B, 10),
-       .rx_af = GPIO_AF7,
-       .tx_af = GPIO_AF7,
-       .bus = APB1,
-       .irqn = USART3_IRQn,
+       /**
+        * @name    UART configuration
+        * 
+        */
+       static const uart_conf_t uart_config[] = {
+           // CN1 connector, Virtual Com Port
+           {.dev = USART3,
+            .rcc_mask = RCC_APB1ENR_USART3EN,
+            .rx_pin = GPIO_PIN(PORT_B, 11),
+            .tx_pin = GPIO_PIN(PORT_B, 10),
+            .rx_af = GPIO_AF7,
+            .tx_af = GPIO_AF7,
+            .bus = APB1,
+            .irqn = USART3_IRQn,
 #ifdef MODULE_PERIPH_DMA
-       .dma_stream = DMA_STREAM_UNDEF,
-       .dma_chan = UINT8_MAX
+            .dma_stream = DMA_STREAM_UNDEF,
+            .dma_chan = UINT8_MAX
 #endif
-      },
-      // CN12 P6(tx), P8(rx) and the Arduino CN7 connector, on D1(tx) and D0(rx) pins
-      {.dev = USART6,
-       .rcc_mask = RCC_APB2ENR_USART6EN,
-       .rx_pin = GPIO_PIN(PORT_G, 9),
-       .tx_pin = GPIO_PIN(PORT_G, 14),
-       .rx_af = GPIO_AF8,
-       .tx_af = GPIO_AF8,
-       .bus = APB2,
-       .irqn = USART6_IRQn,
+           },
+           // Alternate Function 8 (AF8)
+           // On CN12 connector:        TX -> (PORT_C, 6),   RX -> (PORT_C, 7)
+           // On Arduino CN7 connector: TX -> D1(PORT_G, 14),RX -> D0(PORT_G, 9)
+           {.dev = USART6,
+            .rcc_mask = RCC_APB2ENR_USART6EN,
+            .rx_pin = GPIO_PIN(PORT_C, 7),
+            .tx_pin = GPIO_PIN(PORT_C, 6),
+            .rx_af = GPIO_AF8,
+            .tx_af = GPIO_AF8,
+            .bus = APB2,
+            .irqn = USART6_IRQn,
 #ifdef MODULE_PERIPH_DMA
-       .dma_stream = DMA_STREAM_UNDEF,
-       .dma_chan = UINT8_MAX
+            .dma_stream = DMA_STREAM_UNDEF,
+            .dma_chan = UINT8_MAX
 #endif
-      }};
+           }};
 
 #define UART_0_ISR (isr_usart3)
 #define UART_1_ISR (isr_usart6)
 
 #define UART_NUMOF ARRAY_SIZE(uart_config)
-
-
-
 
 #ifdef __cplusplus
 }
