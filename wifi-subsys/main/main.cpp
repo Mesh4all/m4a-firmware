c985f7ad7cdfad8478b7c961899d8ca0b5fdae84
luisan00 <luisan00@hotmail.com>
2022-01-21 15:25:47 +0100

    subsys: initial structure

---
+++ wifi-subsys/main/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 Mesh4all <mesh4all.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @brief       Main firmware file
+ *
+ * @copyright   Copyright (c) 2022 Mesh for all
+ * @author      luisan00 <luisan00@hotmail.com>
+ *
+ */
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+
+extern "C" void app_main(void)
+{
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    esp_restart();
+}
