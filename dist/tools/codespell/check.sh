10ceb2e89deb064e46e0e5e3f2e6984895a8d372
RocioRojas <rociorojas391@gmail.com>
2022-02-02 16:44:07 -0400

    fix issue#120

--- dist/tools/codespell/check.sh
+++ dist/tools/codespell/check.sh
@@ -10,7 +10,7 @@ CODESPELL_OPT=" -c"
 CODESPELL_OPT+=" -q 2"
 CODESPELL_OPT+=" --check-hidden"
 CODESPELL_OPT+=" --ignore-words ${TOOLS}/codespell/ignored_words.txt"
-CODESPELL_OPT+=" --skip=RIOT,dist,.git,css,html,js,m4a.doxyfile"
+CODESPELL_OPT+=" --skip=RIOT,dist,./wifi-subsys/build,.git,css,html,js,m4a.doxyfile"
 ERRORS=$(${CODESPELL_CMD} ${CODESPELL_OPT})
 
 if [ -n "${ERRORS}" ]
