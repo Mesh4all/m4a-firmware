3bd75fb6de62d38fbb46b8617a05d01f21227a0a
eduazocar <eduardo@turpialdev.com>
2022-02-04 21:08:49 -0400

    wifi-subsys: Integration of Uint8_t Cbor Serialization

--- wifi-subsys/components/tools/src/serialization.c
+++ wifi-subsys/components/tools/src/serialization.c
@@ -16,31 +16,31 @@
 
 /**
  *
- * @brief       Cbor encoder and decoder functions to manipulate a group of data.
+ * @brief       Cbor encoder and decoder functions to manipulate a group of
+ * data.
  * @author      xkevin190 <kevinvelasco190@gmail.com>
  * @author      eduazocar <eduardo@turpialdev.com>
  */
 
-#include  <stdio.h>
+#include <stdio.h>
+
 #include "cbor.h"
 #include "esp_log.h"
 #include "serialization.h"
 
-static const char* TAG = "SERIALIZATION";
-
-esp_err_t encode(char* key, int64_t value, uint8_t* cbor_coded, size_t *len)
-{
+esp_err_t encode(char *key, int64_t value, uint8_t *cbor_coded, size_t *len) {
     char buffer[100];
     CborEncoder root_encoder;
     CborError err = CborNoError;
     CborEncoder map_encoder;
 
     // Initialize the outermost cbor encoder and create the element_map
-    cbor_encoder_init(&root_encoder, (uint8_t* )buffer, sizeof(buffer), 0);
+    cbor_encoder_init(&root_encoder, (uint8_t *)buffer, sizeof(buffer), 0);
     err = cbor_encoder_create_map(&root_encoder, &map_encoder, 1);
 
     if (err != CborNoError) {
-        ESP_LOGE(TAG, "Error creating map");
+        ESP_LOGE(__func__, "Error creating map\nReason:%s, Error No:%d", cbor_error_string(err),
+                 err);
     }
 
     // Cbor pass to encode the string Key and the int64 value that belong it//
@@ -48,15 +48,46 @@ esp_err_t encode(char* key, int64_t value, uint8_t* cbor_coded, size_t *len)
     cbor_encode_int(&map_encoder, value);
 
     cbor_encoder_close_container(&root_encoder, &map_encoder);
-    *len = cbor_encoder_get_buffer_size(&root_encoder, (uint8_t* )buffer);
+    *len = cbor_encoder_get_buffer_size(&root_encoder, (uint8_t *)buffer);
 
     memcpy(cbor_coded, buffer, *len);
-    ESP_LOG_BUFFER_HEXDUMP(TAG, cbor_coded, *len, ESP_LOG_INFO);
+    ESP_LOG_BUFFER_HEXDUMP(__func__, cbor_coded, *len, ESP_LOG_INFO);
     return ESP_OK;
 }
 
-esp_err_t decode(uint8_t* buf, char* key, int64_t *value, size_t len)
-{
+esp_err_t encode_uint8(char *key, uint8_t *value, uint8_t *cbor_coded, size_t *len) {
+    char buffer[100];
+    CborEncoder root_encoder;
+    CborError err = CborNoError;
+    CborEncoder map_encoder;
+
+    // Initialize the outermost cbor encoder and create the element_map
+    cbor_encoder_init(&root_encoder, (uint8_t *)buffer, sizeof(buffer), 0);
+    err = cbor_encoder_create_map(&root_encoder, &map_encoder, 1);
+
+    if (err != CborNoError) {
+        ESP_LOGE(__func__, "Error creating the map\nReason:%s, Error No:%d", cbor_error_string(err),
+                 err);
+    }
+
+    // Cbor pass to encode the string Key and the int64 value that belong it//
+    cbor_encode_text_stringz(&map_encoder, key);
+    err = cbor_encode_byte_string(&map_encoder, value, sizeof(value) - 1);
+
+    if (err != CborNoError) {
+        ESP_LOGE(__func__, "Wrong passing string bytes\nReason:%s, Error No:%d",
+                 cbor_error_string(err), err);
+        return ESP_FAIL;
+    }
+    cbor_encoder_close_container(&root_encoder, &map_encoder);
+    *len = cbor_encoder_get_buffer_size(&root_encoder, (uint8_t *)buffer);
+
+    memcpy(cbor_coded, buffer, *len);
+    ESP_LOG_BUFFER_HEXDUMP(__func__, cbor_coded, *len, ESP_LOG_INFO);
+    return ESP_OK;
+}
+
+esp_err_t decode(uint8_t *buf, char *key, void *value, size_t len) {
     CborValue it;
     CborValue map_it;
     CborParser root_parser;
@@ -64,28 +95,42 @@ esp_err_t decode(uint8_t* buf, char* key, int64_t *value, size_t len)
     err = cbor_parser_init(buf, len, 0, &root_parser, &it);
 
     if (err != CborNoError) {
-        ESP_LOGE(TAG, "error parser_init encoding: %d ", err);
+        ESP_LOGE(__func__, "error parser_init encoding\nReason:%s, Error No:%d",
+                 cbor_error_string(err), err);
         return ESP_FAIL;
     }
 
     CborType type = cbor_value_get_type(&it);
 
-        if (type == CborMapType) {
-            err = cbor_value_map_find_value(&it, key, &map_it);
+    if (type == CborMapType) {
+        err = cbor_value_map_find_value(&it, key, &map_it);
 
-            if (err != CborNoError) {
-                ESP_LOGE(TAG, "Error Opening map");
-                return ESP_FAIL;
-            }
-
-            else if (!cbor_value_is_valid(&map_it) && !cbor_value_is_integer(&map_it)) {
-                ESP_LOGI(TAG, "no value");
-                return ESP_FAIL;
-            }
-
-            cbor_value_get_int64(&map_it, value);
-
+        if (err != CborNoError) {
+            ESP_LOGE(__func__, "Error opening map\nReason:%s, Error No:%d", cbor_error_string(err),
+                     err);
+            return ESP_FAIL;
         }
+
+        else if (cbor_value_is_valid(&map_it) && cbor_value_is_integer(&map_it)) {
+            cbor_value_get_int64(&map_it, value);
+            return ESP_OK;
+        }
+
+        else if (cbor_value_is_valid(&map_it) && cbor_value_is_byte_string(&map_it)) {
+            uint8_t *val;
+            size_t len_data;
+            cbor_value_dup_byte_string(&map_it, &val, &len_data, &map_it);
+            memcpy(value, val, len_data);
+            free(val);
+            return ESP_OK;
+        }
+
+        else {
+            ESP_LOGE(__func__, "Data and owner not founded\nReason:%s, Error No:%d",
+                     cbor_error_string(err), err);
+            return ESP_FAIL;
+        }
+    }
 
     return ESP_OK;
 }
