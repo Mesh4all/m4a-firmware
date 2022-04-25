986b8126ebca7f64544b5eeeb6b20ff2468daa49
RocioRojas <62039900+RocioRojas@users.noreply.github.com>
2022-04-25 17:35:06 +0000

    Deploying to gh-pages from @ Mesh4all/m4a-firmware@347b9beace35eef686d2e06941fa6ab895a4705f 🚀

--- group__examples.js
+++ group__examples.js
@@ -1,17 +1,17 @@
 var group__examples =
 [
-    [ "Getting started", "index.html#autotoc_md25", [
-      [ "Getting Started:", "md_examples_ds18b20_README.html#autotoc_md27", null ],
-      [ "Go to the example folder", "md_examples_ds18b20_README.html#autotoc_md28", null ],
-      [ "Compile your code and flash it", "md_examples_ds18b20_README.html#autotoc_md29", null ],
-      [ "First", "md_examples_ds18b20_README.html#autotoc_md30", null ],
-      [ "Last steps", "md_examples_ds18b20_README.html#autotoc_md31", null ]
+    [ "Getting started", "index.html#autotoc_md29", [
+      [ "Getting Started:", "md_examples_ds18b20_README.html#autotoc_md31", null ],
+      [ "Go to the example folder", "md_examples_ds18b20_README.html#autotoc_md32", null ],
+      [ "Compile your code and flash it", "md_examples_ds18b20_README.html#autotoc_md33", null ],
+      [ "First", "md_examples_ds18b20_README.html#autotoc_md34", null ],
+      [ "Last steps", "md_examples_ds18b20_README.html#autotoc_md35", null ]
     ] ],
-    [ "How to test it", "md_examples_external_led_README.html#autotoc_md33", [
-      [ "Go to the example folder", "md_examples_slipdev_README.html#autotoc_md34", null ],
-      [ "Compile your code and flash it", "md_examples_slipdev_README.html#autotoc_md35", null ],
-      [ "First", "md_examples_slipdev_README.html#autotoc_md36", null ],
-      [ "Uart example", "group__tmp__uart.html#autotoc_md37", null ]
+    [ "How to test it", "md_examples_external_led_README.html#autotoc_md37", [
+      [ "Go to the example folder", "md_examples_slipdev_README.html#autotoc_md38", null ],
+      [ "Compile your code and flash it", "md_examples_slipdev_README.html#autotoc_md39", null ],
+      [ "First", "md_examples_slipdev_README.html#autotoc_md40", null ],
+      [ "Uart example", "group__tmp__uart.html#autotoc_md41", null ]
     ] ],
     [ "Analog capacitive moisture sensor", "group__moisture__sensor__example.html", null ],
     [ "Ds18b20", "group__ds18b20.html", null ],
