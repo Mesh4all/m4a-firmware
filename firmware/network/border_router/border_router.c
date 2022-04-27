8fe077dcddf5e723e9882273643441cad52d5b17
Rocio <you@example.com>
2022-04-27 08:10:39 -0400

    Network: border router ipv6 config host

---
+++ firmware/network/border_router/border_router.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022 Mesh4all <mesh4all.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @brief  Border Radio.
+ *
+ * @author  RocioRojas <rociorojas391@gmail.com>
+ */
+#include <stdio.h>
+#include <string.h>
+#include "border_router.h"
+
+int8_t get_wired_iface(void) {
+    int max_ifaces = gnrc_netif_numof();
+    if (max_ifaces > 0) {
+        gnrc_netif_t *iface;
+        iface = gnrc_netif_get_by_type(NETDEV_ANY, NETDEV_INDEX_ANY);
+        if (iface != NULL) {
+            return iface->pid;
+        } else {
+            return -1;
+        }
+    }
+    return -1;
+}
+
+int border_router_add_ipv6(int cast_type, ipv6_addr_t *addr) {
+    uint16_t flags = GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_VALID;
+    uint8_t prefix_len = _IPV6_DEFAULT_PREFIX_LEN;
+    netif_t *iface = NULL;
+    int index = get_wired_iface();
+    int16_t isWired;
+    iface = netif_get_by_id(index);
+    if (cast_type == _ANYCAST || cast_type == _UNICAST || cast_type == _MULTICAST) {
+        if (netif_get_opt(iface, NETOPT_IS_WIRED, 0, &isWired, sizeof(isWired)) > 0) {
+            if (cast_type == _MULTICAST) {
+                if (ipv6_addr_is_multicast(addr)) {
+                    if (netif_set_opt(iface, NETOPT_IPV6_GROUP, 0, addr, sizeof(ipv6_addr_t)) < 0) {
+                        printf("error: unable to join IPv6 multicast group\n");
+                        return -1;
+                    }
+                } else {
+                    return -1;
+                }
+            } else {
+                if (cast_type == _ANYCAST) {
+                    flags |= GNRC_NETIF_IPV6_ADDRS_FLAGS_ANYCAST;
+                }
+                flags |= (prefix_len << 8U);
+                if (netif_set_opt(iface, NETOPT_IPV6_ADDR, flags, addr, sizeof(ipv6_addr_t)) < 0) {
+                    printf("error: unable to add IPv6 address\n");
+                    return -1;
+                }
+            }
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;
+    }
+    return 0;
+}
---
+++ tests/border_router/main.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Mesh4all <mesh4all.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @brief       border router test file
+ *
+ * @author      RocioRojas <Rociorojas391@gmail.com>
+ */
+
+#include <string.h>
+#include <errno.h>
+#include "embUnit.h"
+#include "border_router.h"
+
+void test_border_router_add_ipv6(void) {
+     ipv6_addr_t address = {
+        .u8 = {0},
+    };
+    ipv6_addr_from_str(&address, "2001:db8:1::2");
+    int err = border_router_add_ipv6(_UNICAST,  &address);
+    TEST_ASSERT_EQUAL_INT(0, err);
+}
+
+Test *tests_border_router(void) {
+    EMB_UNIT_TESTFIXTURES(fixtures){
+        new_TestFixture(test_border_router_add_ipv6),
+    };
+
+    EMB_UNIT_TESTCALLER(tests_border_router, NULL, NULL, fixtures);
+
+    return (Test *)&tests_border_router;
+}
+
+int main(void) {
+    TESTS_START();
+    TESTS_RUN(tests_border_router());
+    TESTS_END();
+    return 0;
+}
